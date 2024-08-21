82bed1e4530e2a68fb93cdf162b620fabb1c1582
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2024-08-21 12:07:40 -0500

    async await

---
+++ src/async/asyncawait.js
@@ -0,0 +1,36 @@
+const posts = [
+  { title: "Post One", body: "This is Post One" },
+  { title: "Post Two", body: "This is Post Two" },
+];
+
+function getPosts() {
+  let output = "";
+  setTimeout(() => {
+    posts.forEach((post) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      posts.push(post);
+      const error = false;
+      //const error = true;
+      if (!error) {
+        resolve();
+      } else {
+        reject("Something went wrong");
+      }
+    }, 2000);
+  });
+}
+
+async function init() {
+  await createPost({ title: "Post Three", body: "This is Post Three" });
+  getPosts();
+}
+
+init();
